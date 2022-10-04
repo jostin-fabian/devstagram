6463615a6b174b131e6fb9fa35eecd6bc40db579
jostinOG <Jostinfabian14@gmail.com>
2022-10-04 09:53:09 +0200

    laravel components have been added to the project.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
     //
     public function index(User $user)
     {
-        $posts = Post::where('user_id', $user->id)->paginate(15);
+        $posts = Post::where('user_id', $user->id)->latest()->paginate(15);
 
         return view('dashboard', [
             'user' => $user, 'posts' => $posts
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -90,7 +90,7 @@
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach ($posts as $post )
                     <div>
-                        <a href="{{route('posts.show',['post'=>$post,'user'=>$user])}}">
+                        <a href="{{route('posts.show',['post'=>$post,'user'=>$post->user])}}">
                             <img src="{{asset('uploads') . '/' . $post->image}}" alt="Post Image {{$post->title}}">
                         </a>
 
